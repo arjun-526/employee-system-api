f323171b668782692bb64a19a3a360fc2307d127
Arjun526 <nagarjuna.anke2@gmail.com>
2022-07-09 21:08:32 +0530

    added update method and getById

--- src/main/java/com/valmiki/employee/model/Employee.java
+++ src/main/java/com/valmiki/employee/model/Employee.java
@@ -4,6 +4,10 @@ import lombok.Data;
 
 @Data
 public class Employee {
+	
+	public Employee() {
+		
+	}
 
 	public long getId() {
 		return id;
--- src/main/java/com/valmiki/employee/service/EmployeeServiceImpl.java
+++ src/main/java/com/valmiki/employee/service/EmployeeServiceImpl.java
@@ -11,21 +11,21 @@ import com.valmiki.employee.model.Employee;
 import com.valmiki.employee.repository.EmployeeRepository;
 
 @Service
-public class EmployeeServiceImpl implements EmployeeService{
-	
+public class EmployeeServiceImpl implements EmployeeService {
+
 	private final EmployeeRepository employeeRepository;
-	
+
 	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
-		this.employeeRepository =  employeeRepository;
+		this.employeeRepository = employeeRepository;
 	}
 
 	@Override
 	public Employee createEmployee(Employee employee) {
-	EmployeeEntity employeeEntity =  new EmployeeEntity();
-	
-	BeanUtils.copyProperties(employee, employeeEntity);
-	employeeRepository.save(employeeEntity);
-	
+		EmployeeEntity employeeEntity = new EmployeeEntity();
+
+		BeanUtils.copyProperties(employee, employeeEntity);
+		employeeRepository.save(employeeEntity);
+
 		return employee;
 	}
 
@@ -33,13 +33,34 @@ public class EmployeeServiceImpl implements EmployeeService{
 	public List<Employee> getAllEmployees() {
 		List<EmployeeEntity> employeeEntities = employeeRepository.findAll();
 		List<Employee> employees = employeeEntities.stream()
-				.map(emp-> new Employee(
-						emp.getId(),
-						emp.getFirstName(),
-						emp.getLastName(),
-						emp.getEmailId()))
+				.map(emp -> new Employee(emp.getId(), emp.getFirstName(), emp.getLastName(), emp.getEmailId()))
 				.collect(Collectors.toList());
 		return employees;
+	}
+
+	@Override
+	public boolean deleteEmployee(Long id) {
+		EmployeeEntity employee = employeeRepository.findById(id).get();
+		employeeRepository.delete(employee);
+		return true;
+	}
+
+	@Override
+	public Employee getEmployeeById(Long id) {
+		EmployeeEntity employeeEntity = employeeRepository.findById(id).get();
+		Employee employee = new Employee();
+		BeanUtils.copyProperties(employeeEntity, employee);
+		return employee;
+	}
+
+	@Override
+	public Employee updateEmployee(Long id, Employee employee) {
+		EmployeeEntity employeeEntity = employeeRepository.findById(id).get();
+		employeeEntity.setEmailId(employee.getEmailId());
+		employeeEntity.setFirstName(employee.getFirstName());
+		employeeEntity.setLastName(employee.getLastName());
+	       employeeRepository.save(employeeEntity);
+		return employee;
 	}
 
 }
